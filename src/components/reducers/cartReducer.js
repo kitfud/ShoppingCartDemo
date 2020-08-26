a77df9f3647016094320901baddb6bb32d27a786
Kit Fuderich <kitfuderich@gmail.com>
2020-08-26 15:32:24 -0700

    changed pricing on items

--- src/components/reducers/cartReducer.js
+++ src/components/reducers/cartReducer.js
@@ -10,12 +10,12 @@ import Item6 from '../images/micro5.jpg'
 
 const initState = {
     items: [
-        {id:1,title:'Green Microgreens', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:Item1},
-        {id:2,title:'Basil Microgreens', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:80,img: Item2},
-        {id:3,title:'Beet Microgreens', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:120,img: Item3},
-        {id:4,title:'Swiss Chard Microgreens', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:260,img:Item4},
-        {id:5,title:'Test Microgreens', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:160,img: Item5},
-        {id:6,title:'Test2 Microgreens', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:90,img: Item6}
+        {id:1,title:'Green Microgreens', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:10,img:Item1},
+        {id:2,title:'Basil Microgreens', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:8,img: Item2},
+        {id:3,title:'Beet Microgreens', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:8,img: Item3},
+        {id:4,title:'Swiss Chard Microgreens', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:8,img:Item4},
+        {id:5,title:'Test Microgreens', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:8,img: Item5},
+        {id:6,title:'Test2 Microgreens', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:9,img: Item6}
     ],
     addedItems:[],
     total: 0
